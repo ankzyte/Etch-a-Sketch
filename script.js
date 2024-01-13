7e8b59c06dcb22ae4c39931ae029cc6ea0d363bb
ankzyte <ankitsinghbl2003@gmail.com>
2024-01-13 23:54:26 +0530

    add logic

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 let grids = 16;
 let container = document.querySelector(".container");
+let isMouseDown = false;
+let userInput = document.querySelector("#grid");
+let confirmButton = document.querySelector("#confirm");
 
 let createGrids=()=>{
     for(let i=0;i<grids;i++){
         for(let j=0;j<grids;j++){
             let squareDiv = document.createElement("div");
             let heightWidth = 640/grids;
-            squareDiv.style.width = `${heightWidth} px`;
-            squareDiv.style.height = `${heightWidth} px`;
+            squareDiv.style.width = `${heightWidth}px`;
+            squareDiv.style.height = `${heightWidth}px`;
             container.append(squareDiv);
+            squareDiv.addEventListener("mousedown",()=>{isMouseDown=true});
+            squareDiv.addEventListener("mouseup",()=>{isMouseDown=false});
+            squareDiv.addEventListener("mouseenter",draw);            
         }
     }
+    console.log(isMouseDown)
+};
+
+let clearGrids=()=>{
+    container.innerHTML="";
+    grids=userInput.value;
+    createGrids();
+}
+
+let draw=(e)=>{
+    if(isMouseDown){
+        e.currentTarget.style.backgroundColor="black";
+    }    
 };
 
 createGrids();
+confirmButton.addEventListener("click",clearGrids);
